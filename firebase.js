87c0e3f39cac4369f370ed5923592b6ceb612cc4
Qusai007 <qusaijohar.11@gmail.com>
2024-11-25 18:00:01 -0500

    Update firebase

--- firebase.js
+++ firebase.js
@@ -1,29 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { initializeAuth } from 'firebase/auth/react-native';
-import { getFirestore } from 'firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getReactNativePersistence } from 'firebase/auth/react-native';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAF4pOHorUhXtepTi44xCL2esqBya37H4k",
   authDomain: "eventorganizerapp-1f5ba.firebaseapp.com",
   projectId: "eventorganizerapp-1f5ba",
-  storageBucket: "eventorganizerapp-1f5ba.appspot.com",
+  storageBucket: "eventorganizerapp-1f5ba.firebasestorage.app",
   messagingSenderId: "804772407031",
   appId: "1:804772407031:web:90fe40110d8272364155f2",
   measurementId: "G-4LRSGNG0PX"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Auth with AsyncStorage persistence
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-
-// Initialize Firestore
-const firestore = getFirestore(app);
-
-export { auth, firestore };
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
