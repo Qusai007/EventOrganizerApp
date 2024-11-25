6ad8f250bcfdd6fa0c3a7f7a64884c5aeaddfb36
Qusai007 <qusaijohar.11@gmail.com>
2024-11-25 14:21:39 -0500

    FavoriteEventsScreen Created

---
+++ screens/FavoriteEventsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function FavoriteEventsScreen() {
+  return (
+    <View>
+      <Text>Favorite Events</Text>
+    </View>
+  );
+}
