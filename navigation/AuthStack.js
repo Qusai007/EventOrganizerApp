4ef6bded991eb3bf896da9635e3466e6f35943a2
Qusai007 <qusaijohar.11@gmail.com>
2024-11-25 14:33:12 -0500

    Update AuthStack

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -8,10 +8,22 @@ const Stack = createStackNavigator();
 
 export default function AuthStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Main" component={MainStack} options={{ headerShown: false }} />
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUpScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Main"
+        component={MainStack}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
