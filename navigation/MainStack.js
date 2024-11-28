998b1996c6fcbaa660a13df1e28f1a89618de003
Qusai007 <qusaijohar.11@gmail.com>
2024-11-27 19:59:55 -0500

    Update MainStack

--- navigation/MainStack.js
+++ navigation/MainStack.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons"; // Import Ionicons for icons
 import EventListScreen from "../screens/EventListScreen";
-import AddEditEventScreen from "../screens/AddEditEventScreen";
 import FavoriteEventsScreen from "../screens/FavoriteEventsScreen";
+import AddEditEventScreen from "../screens/AddEditEventScreen";
+import { auth } from "../firebase";
+import { Button } from "react-native";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -11,15 +14,46 @@ const Stack = createStackNavigator();
 function EventStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="EventList" component={EventListScreen} />
-      <Stack.Screen name="AddEditEvent" component={AddEditEventScreen} />
+      <Stack.Screen
+        name="EventList"
+        component={EventListScreen}
+        options={{
+          headerRight: () => (
+            <Button title="Logout" onPress={() => auth.signOut()} />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="AddEditEvent"
+        component={AddEditEventScreen}
+        options={({ route }) => ({
+          title: route.params?.eventId ? "Edit Event" : "Add Event",
+        })}
+      />
     </Stack.Navigator>
   );
 }
 
 export default function MainStack() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Events") {
+            iconName = focused ? "calendar" : "calendar-outline";
+          } else if (route.name === "Favorites") {
+            iconName = focused ? "heart" : "heart-outline";
+          }
+
+          // Return the Ionicons component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="Events" component={EventStack} />
       <Tab.Screen name="Favorites" component={FavoriteEventsScreen} />
     </Tab.Navigator>
